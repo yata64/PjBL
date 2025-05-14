9698c3eaef7d3e90b714622fd2434e1f53c9fb31
yata64 <joaomarioys@gmail.com>
2025-05-14 10:58:25 -0300

    Update Pessoa.java
    Criação da classe abstrata Pessoa com dois atributos (nome e cpf), também foi criado dentro da classe o método exibir_informacoes().

---
+++ Pessoa.java
@@ -0,0 +1,19 @@
+public abstract class Pessoa{
+    protected String nome;
+    protected String cpf;
+
+    public Pessoa(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;        
+    }
+
+    public abstract void exibir_informacoes();
+
+    public String get_nome(){
+        return nome;
+    }
+
+    public String get_cpf(){
+        return cpf;
+    }
+}
