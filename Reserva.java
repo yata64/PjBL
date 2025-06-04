80c34d4390da183d4f49e10fbeafa2b222a41cfd
yata64 <joaomarioys@gmail.com>
2025-06-04 20:07:58 -0300

    Criada a classe Reserva
    foi definido a classe e os atributos e metodos da classe criada, de acordo com as reservas de hotel

--- Reserva.java
+++ Reserva.java
@@ -1,3 +1,30 @@
 public class Reserva {
-    
+    private int numero;
+    private String tipo;
+    private float preco;
+    private String cliente;
+
+    public Reserva(int numero, String tipo, float preco, String cliente){
+        this.numero = numero;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.cliente = cliente;
+    }
+
+    public int get_numero(){
+        return numero;
+    }
+
+    public String get_tipo(){
+        return tipo;
+    }
+
+    public float get_preco(){
+        return preco;
+    }
+
+    public String cliente(){
+        return cliente;
+    }
+
 }
