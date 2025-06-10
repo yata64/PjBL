1db3aacafb52d2497d7ec56b0729b199daa946c0
yata64 <joaomarioys@gmail.com>
2025-06-10 20:29:53 -0300

    Classe Hotel
    Foi adicionado o encapsulamento que não tinha sido feito, feito os métodos para adicionar reservas e quartos, adicionado também busca e listagem de quartos e quartos livres respectivamente.

--- Hotel.java
+++ Hotel.java
@@ -12,4 +12,48 @@ public class Hotel{
         this.reservas = new ArrayList<>();
         this.quartos = new ArrayList<>();
     }
+
+    public String get_nome(){
+        return nome;
+    }
+
+    public String get_endereco(){
+        return endereco;
+    }
+
+    public ArrayList<Reserva> get_reservas(){
+        return reservas;
+    }
+
+    public ArrayList<Quarto> get_quartos(){
+        return quartos;
+    }
+
+    public void add_reserva(Reserva reserva){
+        reservas.add(reserva);
+    }
+
+    public void add_quarto(Quarto quarto){
+        quartos.add(quarto);
+    }
+
+    public Quarto buscar_quarto(int numero){
+        for (Quarto q : quartos){
+            if (q.get_numero() == numero){
+                return q;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Quarto> quartos_livres(){
+        ArrayList<Quarto> livres = new ArrayList<>();
+        for (Quarto q : quartos){
+            if (q.get_ocupacao() == Quarto.ocupacao.livre){
+                livres.add(q);
+            }
+        }
+        return livres;
+    }
+    
 }
