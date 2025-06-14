4393f82d77706a31204868e24695918f5ad9b14c
yata64 <joaomarioys@gmail.com>
2025-06-14 20:51:55 -0300

    Hotel
    adicionado uma função para ler arquivos .txt e fiz a persistencia de objetos

--- Hotel.java
+++ Hotel.java
@@ -1,16 +1,25 @@
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Hotel{
     private String nome;
     private String endereco;
     private ArrayList<Reserva> reservas;
     private ArrayList<Quarto> quartos;
+    private ArrayList<Funcionario> funcionarios;
 
     public Hotel(String nome, String endereco){
         this.nome = nome;
         this.endereco = endereco;
         this.reservas = new ArrayList<>();
         this.quartos = new ArrayList<>();
+        this.funcionarios = new ArrayList<>();
+
     }
 
     public String get_nome(){
@@ -37,6 +46,10 @@ public class Hotel{
         quartos.add(quarto);
     }
 
+    public void add_funcionario(Funcionario funcionario){
+        funcionarios.add(funcionario);
+    }
+
     public Quarto buscar_quarto(int numero){
         for (Quarto q : quartos){
             if (q.get_numero() == numero){
@@ -56,5 +69,36 @@ public class Hotel{
         }
         return livres;
     }
+
+    public void carregar_quartos(String caminho) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader(caminho));
+        String linha;
+
+        while ((linha = br.readLine()) != null){
+            String[] partes = linha.split(",");
+            int numero = Integer.parseInt(partes[0]);
+            String tipo = partes[1];
+            int capacidade = Integer.parseInt(partes[2]);
+            float diaria = Float.parseFloat(partes[3]);
+            Quarto.ocupacao status = Quarto.ocupacao.valueOf(partes[4]);
+
+            Quarto quarto = new Quarto(numero, tipo, capacidade, diaria, status);
+            this.add_quarto(quarto);
+        }
+
+        br.close();
+    }
+
+    public void salvar_reservas(String arquivo) throws Exception {
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(arquivo));
+        out.writeObject(reservas);
+        out.close();
+    }
+
+    public void carregar_reservas(String arquivo) throws Exception{
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(arquivo));
+        reservas = (ArrayList<Reserva>) in .readObject();
+        in.close();
+    } 
     
 }
